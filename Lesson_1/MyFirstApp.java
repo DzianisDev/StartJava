eed7d324aac3cc75f6466a9c6b7df5a4c18f29c1
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-04-05 14:34:41 +0300

    Initial repository

---
+++ Lesson_1/MyFirstApp.java
@@ -0,0 +1,6 @@
+package Lesson_1;
+public class MyFirstApp  { //Initial Java Class
+    public static void main(String[] args){ // Start Method, entry point
+        System.out.println("Hello, world");
+    }
+}
