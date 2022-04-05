8f61e6fa0fe89dfa6a943725487ebf9d4126e9f7
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-04-05 15:44:25 +0300

    First commit FIX

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -1,4 +1,3 @@
-package Lesson_1;
 public class MyFirstApp  { //Initial Java Class
     public static void main(String[] args){ // Start Method, entry point
         System.out.println("Hello, world");
