83e03df79132c32ff6706a8d226f221b86ee5a00
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-04-12 13:09:14 +0300

    HW_1 simple Game app guess the number

---
+++ Lesson_1/HW_1/MyFirstGame.java
@@ -0,0 +1,19 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int hiddenNumber = 39;
+        int playerNumber = 2;
+
+        while (hiddenNumber != playerNumber) {
+            if (playerNumber > hiddenNumber) {
+                System.out.println("Число " + playerNumber + " больше чем загадал компьютер");
+                playerNumber -=11;
+            } else if (playerNumber < hiddenNumber) {
+                System.out.println("Число "+playerNumber+" меньше чем загадал компьютер");
+                playerNumber +=41;
+            }
+        }
+        System.out.println("Поздравляю, Вы угадали число! \n" 
+        + "Ваше число: " + playerNumber + "\n"
+        + "Загаданное число: " + hiddenNumber);
+    }
+}
