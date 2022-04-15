fa72dbb0697c47efeacdc0637f5a06a12161f47c
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-04-15 13:35:19 +0300

    HW_2 Create CalculatorTest.java, modified Calculator.java

--- Lesson_1/HW_1/Calculator.java
+++ Lesson_1/HW_1/Calculator.java
@@ -1,32 +1,63 @@
 public class Calculator {
-    public static void main(String[] args) {
         /*
         * над целыми положительными числами программа 
         * должна уметь выполнять следующие вычисления: +, -, *, /, ^, %
         */
-        int firstNumber = 20;
-        int secondNumber = 6;
-        int result = 0;
-        char mathOperation = '^';
+      private int firstNumber;
+      private int secondNumber;
+      private int result = 0;
+      private char mathOperation;
 
-        if (mathOperation == '+') {
-            result = firstNumber + secondNumber;
-        } else if (mathOperation == '-') {
-            result = firstNumber - secondNumber;
-        } else if (mathOperation == '*') {
-            result = firstNumber * secondNumber;
-        } else if (mathOperation == '/') {
-            result = firstNumber / secondNumber;
-        } else if (mathOperation == '^') {
-            result = 1;
-            for (int i = 0; i < secondNumber; i++) {
-                result *= firstNumber;
+      public void setFirstNumber(int firstNumber) {
+          this.firstNumber = firstNumber;
+      }
+
+      public void setSecondNumber(int secondNumber) {
+          this.secondNumber = secondNumber;
+      }
+
+      public void setMathOperation(char mathOperation) {
+          this.mathOperation = mathOperation;
+      }
+
+      public int calculate() {
+          switch(this.mathOperation) {
+              case '+': return add();
+              case '-': return substract();
+              case '*': return multiply();
+              case '/': return divide();
+              case '^': return exponentiate();
+              case '%': return divideByReminder();
+              default: System.out.println("Invalid math operator!");
+                return -1;
             }
-        } else if (mathOperation == '%') {
-            result = firstNumber % secondNumber;
-        } else {
-            System.out.println("Введен некорректный математический оператор");
+      }
+
+      private int add() {
+          return this.firstNumber + this.secondNumber;
         }
-        System.out.println(firstNumber + " " + mathOperation + " " + secondNumber + " = " + result);
-    }    
+
+      private int substract() {
+          return  this.firstNumber - this.secondNumber;
+      }
+
+      private int multiply() {
+          return this.firstNumber * this.secondNumber;
+      }
+
+      private int divide() {
+          return this.firstNumber / this.secondNumber;
+      }
+
+      private int exponentiate() {
+          int result = 1;
+          for (int i = 0; i < secondNumber; i++) {
+              result *= firstNumber;
+          }
+          return result;
+      }
+
+      private int divideByReminder() {
+          return this.firstNumber % this.secondNumber;
+      }    
 }
---
+++ Lesson_1/HW_1/CalculatorTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class CalculatorTest {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calc = new Calculator();
+        String choice;
+        
+        do {
+            System.out.println("Введите первое число: ");
+            calc.setFirstNumber(scanner.nextInt());
+            System.out.println("Введите второе число: ");
+            calc.setSecondNumber(scanner.nextInt());
+            System.out.println("Введите математическую операцию");
+            calc.setMathOperation(scanner.next().charAt(0));
+            System.out.println(calc.calculate());
+
+            do {
+                System.out.println("Желаете продолжить вычисления? [yes/no]");
+                choice = scanner.next();
+            } while (!choice.equals("no") && !choice.equals("yes"));
+        } while (!choice.equals("no"));
+
+    }
+}
